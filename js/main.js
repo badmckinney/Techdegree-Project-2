6203f90b98331a076bd7e8df6b07f4ccb6341c1d
Brad McKinney <brad.mckin@gmail.com>
2018-05-21 20:53:47 -1000

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const masterList = document.querySelector('.student-list');
+
+//populates page with a group of 10 students based on their respective page numbers
+function showPage(page, studentList) {
+  masterList.hide();
+
+}
+
+function appendPageLinks(studentList) {
+
+}
