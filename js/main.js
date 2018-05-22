65013ced96979787aea41072c4670adc885ea0c6
Brad McKinney <brad.mckin@gmail.com>
2018-05-21 21:35:13 -1000

    Added showPage function

--- js/main.js
+++ js/main.js
@@ -3,9 +3,14 @@ const masterList = document.querySelector('.student-list');
 //populates page with a group of 10 students based on their respective page numbers
 function showPage(page, studentList) {
   masterList.hide();
-
+  for (let i = 0; i < studentList.length; i += 1) {
+    if (i >= (page - 1) * 10 && <= (page * 10) - 1) {
+      studentList[i].show();
+    }
+  }
 }
 
+//
 function appendPageLinks(studentList) {
-
+  let totalStudents = studentList.length;
 }
