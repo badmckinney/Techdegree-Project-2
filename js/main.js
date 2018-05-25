e986f889c06de5c91837c5a0377d2e7906893d61
Brad McKinney <brad.mckin@gmail.com>
2018-05-24 20:27:50 -1000

    added search function

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ let pagUL;
 let numberOfPages;
 let anchors = document.getElementsByTagName('a');
 const pageDiv = document.querySelector('.page');
+let searchBar;
 
 //populates page with a group of 10 students based on their respective page numbers
 function showPage(page, studentList) {
@@ -21,7 +22,7 @@ function showPage(page, studentList) {
 }
 
 //
-function appendPageLinks(studentlist) {
+function appendPageLinks(studentList) {
   numberOfPages = Math.ceil(totalStudents / 10);
   pagLinks = document.createElement('div');
   pagLinks.className = 'pagination';
@@ -33,7 +34,7 @@ function appendPageLinks(studentlist) {
     pagUL.innerHTML += '<li><a href="#" class="inactive">' + [i + 1] + '</a></li>';
   }
   pagUL.addEventListener("click", (e) => {
-    showPage(e.target.textContent, studentList);
+    showPage((e.target.textContent), studentList);
     for (i = 0; i < numberOfPages; i += 1) {
       anchors[i].className = "inactive";
     }
@@ -41,6 +42,30 @@ function appendPageLinks(studentlist) {
   });
 }
 
+function createSearch() {
+  searchBar = document.createElement('div');
+  searchBar.className = 'student-search';
+  let html = "";
+  html += '<input type="text" id="userInput" onkeyup="search()" placeholder="Search for students..."><button>Search</button>';
+  searchBar.innerHTML = html;
+  document.querySelector('.page-header').appendChild(searchBar);
+}
+
+function search() {
+  filter = userInput.value.toUpperCase();
+  for (i = 0; i < students.length; i += 1) {
+    eachStudent = students[i].getElementsByTagName('h3')[0];
+    if (eachStudent.innerHTML.toUpperCase().includes(filter)) {
+      students[i].style.display = "block";
+    } else {
+      students[i].style.display = "none";
+    }
+  }
+}
+
+
+
+createSearch();
 showPage(1, students);
 appendPageLinks(students);
 anchors[0].className = "active";
